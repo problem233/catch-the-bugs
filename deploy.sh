72958499e3089c26dbccddc8314467c5c0a912f7
problem233 <problem233@outlook.com>
2019-07-12 22:16:08 +0800

    edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-echo 'cleaning...'
-npm run clean
-
-echo 'initializing git repo...'
-mkdir dist
-pushd dist
-  git init
-  git checkout -b gh-pages
-popd
-
 echo 'building...'
 npm run build
 
-echo 'committing and pushing...'
 readonly remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 readonly remote=${remote_branch%%/*}
 readonly remote_url=$(git remote get-url $remote)
 pushd dist
+  echo 'initializing git repo...'
+  git init
+  git checkout -b gh-pages
+  echo 'committing and pushing...'
   git add *
   git commit -m "update gh-pages"
   git push -f $remote_url gh-pages
